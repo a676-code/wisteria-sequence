f4cb350f2170e61c947bffbd7676733f8b942b9d
a676-code <109701008+thisusernameisaparadox@users.noreply.github.com>
2023-03-26 18:15:51 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# wisteria-seqeunce
+This script generates the [Wisteria sequence](https://www.youtube.com/watch?v=o8c4uYnnNnc). 
+
+# Plotting with Seaborn
+## Basic Scatterplots
+Scatterplot of 1000 numbers:
+![1000 numbers](1000.png)
+Scatterplot of 10,000 numbers:
+![10,000 numbers](10000.png)
+Scatterplot of 100,000 numbers:
+![100,000 numbers](100000.png)
+
+## Plotting the Average as $n \to \infty$
+Scatterplot of the first 10 averages:
+![10 averages](average_10.png)
+Scatterplot of the first 100 averages:
+![100 averages](average_100.png)
+Scatterplot of the first 1,000 averages:
+![1000 averages](average_1000.png)
+Scatterplot of the first 10,000 averages:
+![10,000 averages](average_10000.png)
+Scatterplot of the first 100,000 averages:
+![100,000 averages](average_100000.png)
---
+++ wisteria-sequence.py
@@ -0,0 +1,108 @@
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# returns the product of the nonzero digits in num
+def productNonzeroDigits(num):
+    string = [char for char in str(num)]
+    product = 1
+    for s in string:
+        if int(s) != 0:
+            product *= int(s)
+    return product
+
+# generates the wisteria sequence up to the n-th number
+def generate_wisteria(n):
+    sequence = []
+    i = 1
+    while len(sequence) < n:
+        sequence.append(i - productNonzeroDigits(i))
+        i += 1
+    return sequence
+
+# Basic scatterplots
+n = 100
+sequence = generate_wisteria(n)
+df = pd.DataFrame(sequence, columns=['Number'])
+df['index'] = [i for i in range(n)]
+sns.scatterplot(x="index",y = "Number", data=df)
+plt.show()
+
+n = 1000
+sequence = generate_wisteria(n)
+df = pd.DataFrame(sequence, columns=['Number'])
+df['index'] = [i for i in range(n)]
+sns.scatterplot(x="index",y = "Number", data=df)
+plt.show()
+
+n = 10000
+sequence = generate_wisteria(n)
+df = pd.DataFrame(sequence, columns=['Number'])
+df['index'] = [i for i in range(n)]
+sns.scatterplot(x="index",y = "Number", data=df)
+plt.show()
+
+n = 100000
+sequence = generate_wisteria(n)
+df = pd.DataFrame(sequence, columns=['Number'])
+df['index'] = [i for i in range(n)]
+sns.scatterplot(x="index",y = "Number", data=df)
+plt.show()
+
+# Plots of Averages
+n = 100
+sequence = generate_wisteria(n)
+average_sequence = []
+total = 0
+for i, s in enumerate(sequence):
+    total += s
+    average = total / (i + 1)
+    average_sequence.append(average)
+
+df = pd.DataFrame(average_sequence, columns=["Average"])
+df['index'] = [i for i in range(n)]
+sns.scatterplot(x="index", y="Average", data=df)
+plt.show()
+
+n = 1000
+sequence = generate_wisteria(n)
+average_sequence = []
+total = 0
+for i, s in enumerate(sequence):
+    total += s
+    average = total / (i + 1)
+    average_sequence.append(average)
+
+df = pd.DataFrame(average_sequence, columns=["Average"])
+df['index'] = [i for i in range(n)]
+sns.scatterplot(x="index", y="Average", data=df)
+plt.show()
+
+n = 10000
+sequence = generate_wisteria(n)
+average_sequence = []
+total = 0
+for i, s in enumerate(sequence):
+    total += s
+    average = total / (i + 1)
+    average_sequence.append(average)
+
+df = pd.DataFrame(average_sequence, columns=["Average"])
+df['index'] = [i for i in range(n)]
+sns.scatterplot(x="index", y="Average", data=df)
+plt.show()
+
+n = 100000
+sequence = generate_wisteria(n)
+average_sequence = []
+total = 0
+for i, s in enumerate(sequence):
+    total += s
+    average = total / (i + 1)
+    average_sequence.append(average)
+
+df = pd.DataFrame(average_sequence, columns=["Average"])
+df['index'] = [i for i in range(n)]
+sns.scatterplot(x="index", y="Average", data=df)
+plt.show()
